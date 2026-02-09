23db348b10197d07c26b8321814b3552b95eb0a9
Chris Spillane <chris.spillane@gmail.com>
2026-02-09 08:43:28 +0000

    Update gendom.sh
    added more TLDs

--- gendom.sh
+++ gendom.sh
@@ -3,7 +3,7 @@ for i in $(seq -w 1 10000); do     # we use seq command for loop
 number=$(( (RANDOM % (15 - 3 + 1)) + 3 ))
 Z=`head -c 2048 /dev/urandom | md5sum | head -c $number;`
 #Z=$(openssl rand -base64 12 | fold -bw20);      # generate a random base-64 string of length 'i'  
-randomtld=(.com .net .co .uk .xyz .jp)				 # possible TLDs list, you can add or remove as needed   
+randomtld=(.com .net .cn .in .co .uk .xyz .jp)				 # possible TLDs list, you can add or remove as needed   
 x="${randomtld[RANDOM%${#randomtld[@]}]}"		#choose a random TLD                
 echo "$Z$x A" >> domains;     # append it to the file and write a newline, remove 'A' if you don't want the record type appended  
 done
